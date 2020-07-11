c5a3cc108944b92f4b14920f7eb8e747a775ea8d
polyglotDataNerd <gerardnino@gmail.com>
2020-07-11 07:52:25 -0700

    added Producer skeleton

---
+++ src/main/java/com/poly/poc/kafka/producer/Producer.java
@@ -0,0 +1,15 @@
+package com.poly.poc.kafka.producer;
+
+public class Producer {
+
+    /**
+    *Documentation and tutorial:
+        1.https://www.tutorialspoint.com/apache_kafka/apache_kafka_simple_producer_example.htm
+        2.https://kafka.apache.org/0110/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
+     */
+
+    public static void main (String[] ...args) {
+
+    }
+
+}
